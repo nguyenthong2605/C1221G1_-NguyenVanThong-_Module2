52a444ab5e0403f2ca5a7b03d87014926e1a711a
unknown <you@example.com>
2022-02-16 11:07:39 +0700

    java

---
+++ module2/src/ss1_introduction_to_java/thuc_hanh/HienThiThoiGianCuaHeThong.java
@@ -0,0 +1,12 @@
+package ss1_introduction_to_java.thuc_hanh;
+
+
+import java.util.Date;
+
+public class HienThiThoiGianCuaHeThong {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
