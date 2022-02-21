8e38543651cbdc10a7aab86a749086310502c8ce
unknown <you@example.com>
2022-02-21 17:12:24 +0700

    java

---
+++ module2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/XayDungLopQuadraticEquation/QuadracticEquation.java
@@ -0,0 +1,35 @@
+package ss4_lop_va_doi_tuong_trong_java.bai_tap.XayDungLopQuadraticEquation;
+
+public class QuadracticEquation {
+    double a, b, c, delta, r1, r2;
+
+    public QuadracticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return delta = Math.pow(b, 2) - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        return r1 = (-b + Math.sqrt(delta)) / 2 * a;
+    }
+
+    public double getRoot2() {
+        return r2 = (-b - Math.sqrt(delta)) / 2 * a;
+    }
+}
