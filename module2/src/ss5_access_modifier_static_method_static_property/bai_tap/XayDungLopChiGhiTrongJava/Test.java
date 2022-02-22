0e25c7744120b9034bf87998dc40b322a4dc60e2
unknown <you@example.com>
2022-02-22 14:00:42 +0700

    java

---
+++ module2/src/ss5_access_modifier_static_method_static_property/bai_tap/XayDungLopChiGhiTrongJava/Test.java
@@ -0,0 +1,11 @@
+package ss5_access_modifier_static_method_static_property.bai_tap.XayDungLopChiGhiTrongJava;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1);
+        student1.setName("Thong");
+        student1.setClasses("C1221G1");
+        System.out.println(student1);
+    }
+}
